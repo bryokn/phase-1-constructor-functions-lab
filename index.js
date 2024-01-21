c6a57d9fc44b8f38b4a117d5f703f396436835fa
Brian Kipkirui <bryokn@gmail.com>
2024-01-21 19:06:56 +0300

    Done

---
+++ index.js
@@ -0,0 +1,26 @@
+// Scooter constructor
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+// Driver constructor
+function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+// PickupLocation constructor
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+}
+
+// Creating new instances
+let myScooter = new Scooter(2024, 'red', 'scoot v2');
+let myDriver = new Driver('Brian Kipkirui', 30, '5 years');
+let myPickupLocation = new PickupLocation('Moi Avenue', 'Nairobi');
+
+
